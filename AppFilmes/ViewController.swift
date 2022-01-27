c691067b18b927b10afa075e28233ad95b3cdc11
Leandro BS <joleandrobruno@gmail.com>
2022-01-27 15:32:58 -0800

    Initial Commit

---
+++ AppFilmes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppFilmes
+//
+//  Created by Dev on 27/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
